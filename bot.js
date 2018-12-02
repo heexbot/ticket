ae82c7fd20b88c03c87f996210d1dc9fdb151e53
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-02 19:05:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ if(command == prefix + 'Bc') { // الكوماند !bc
         
 
 
-        message.channel.send(`** Are you sure you want to send the message? :mailbox_with_mail: **`).then(msg => msg.delete(5000));
+        message.channel.send(`**Are you sure you want to send the message? :mailbox_with_mail: **`).then(msg => msg.delete(5000));
       
               let bcSure = new Discord.RichEmbed()
 
@@ -91,6 +91,81 @@ message.delete();
     }
 
 });
+
+
+
+var prefix = "d";
+
+var adminprefix = "Fbc";
+
+const developers = ["323888904602124299","441554686399741962"]
+
+client.on('message', message => {
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+
+      if (!developers.includes(message.author.id)) return;
+
+      
+
+  if (message.content.startsWith(adminprefix + 'setg')) {
+
+    client.user.setGame(argresult);
+
+      message.channel.send(`**تم تغيير حالتك الى** **${argresult}**`)
+
+  } else 
+
+     if (message.content === (adminprefix + "leave")) {
+
+    message.guild.leave(); 
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'setw')) {
+
+  client.user.setActivity(argresult, {type:'WATCHING'});
+
+      message.channel.send(`**تم تغيير الواتشنق الى** **${argresult}**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'setl')) {
+
+  client.user.setActivity(argresult , {type:'LISTENING'});
+
+      message.channel.send(`**تم تغير اللستنق الى ** **${argresult}**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'sets')) {
+
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+
+      message.channel.send(`**تم تغيير الستريمنق الى** **${argresult}**`)
+
+  }
+
+  if (message.content.startsWith(adminprefix + 'setname')) {
+
+  client.user.setUsername(argresult).then
+
+      message.channel.send(`**تم تغيير اسمك الى ** ..**${argresult}** `)
+
+} else
+
+if (message.content.startsWith(adminprefix + 'setava')) {
+
+  client.user.setAvatar(argresult);
+
+    message.channel.send(`**تم تغيير صورتك الى**:**${argresult}** `);
+
+}
+
+});
+ 
+
+
 
 
 
