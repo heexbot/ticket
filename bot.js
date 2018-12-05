bb498e54c2f72f83b4bb3cc56678e1bf4327f775
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-05 20:59:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,52 +4,6 @@ const client = new Discord.Client();
 
  
 
-client.on('message', message => {
-              if(!message.channel.guild) return;
-    if(message.content.startsWith('Wbc')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return: 
-   if(message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`');  //7md
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "DgPro-BC";
-    let request = `Requested By ${message.author.username}`;  //7md
-    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))  //7md
-    .then(() =>msg.react('✅'))  //7md
-
-
-
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });  //7md
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });  //7md
-    reaction1.on("collect", r => {
-    message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));  //7md
-    message.guild.members.forEach(m => {
-    var bc = new  
-       Discord.RichEmbed()
-       .setColor('RANDOM')
-       .setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)  //7md
-       .addField('الرساله', args)  //7md
-       .setThumbnail(message.author.avatarURL)  //7md
-       .setFooter(copy, client.user.avatarURL); //7md
-    m.send({ embed: bc })
-    msg.delete();  //7md
-    })
-    })
-    reaction2.on("collect", r => {  //7md
-    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));   //7md
-    msg.delete();  //7md  
-    })  //7md
-    }) //7md
-    }  //7md
-    }) //7md
- 
-
-
- 
-
  
 
  
@@ -131,6 +85,40 @@ if (message.content.startsWith(adminprefix + 'setava')) {
 
 });
  
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+        if(message.channel.type === 'dm') return message.reply('This Command Is Only For Server!');
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+}
+});
+
+
+
+
+
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "رسالة")) {
+    let i = client.users.size;
+    if(message.author.id !== '323888904602124299') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
+    });
+  }
+});
 
 
 
