f5bc6ffc7743723e21885fe1567a85970276995f
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-06 13:55:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,44 @@ const client = new Discord.Client();
 
  
 
- 
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+        if(message.channel.type === 'dm') return message.reply('This Command Is Only For Server!');
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+}
+});
+
+
+
+
+
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "bcall")) {
+    let i = client.users.size;
+    if(message.author.id !== '323888904602124299') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
+    });
+  }
+});
+
+__
+كود برودكاست مدموج مع كود bcall
+من صنعي طبعا
+@here l @everyone 
         
         
 
